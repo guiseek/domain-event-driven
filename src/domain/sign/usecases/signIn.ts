deb433d5aa7670e8ce1c9237bfa0fd42584dc9f1
Gui Seek <guiseek@gmail.com>
2022-05-03 22:36:51 -0300

    poc

--- src/domain/sign/usecases/signIn.ts
+++ src/domain/sign/usecases/signIn.ts
@@ -3,22 +3,27 @@ import useCase from '../../../core/hooks/useCase'
 import { SignIn, SignInSuccess } from '../api'
 import { sign } from '../event'
 
+const validadeEmail = ({ email }: SignIn) => {
+  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email)
+}
+
 sign.on('signIn', (value) => {
-  useCase(value, (value) => {
-  
-    repository
-      .post<SignInSuccess, SignIn>('sign-in', value)
-      .then(({ message }) => {
-        sign.emit('signInSuccess', { message })
-      })
-  
-    }, (value) => {
-  
-    return value.email.indexOf('@') !== -1
-  
-  }, (abort) => {
-  
-    abort.dispatchEvent(new Event('abort'))
-  
-  })
+  useCase(
+    value,
+    (value) => {
+      // repository
+      //   .post<SignInSuccess, SignIn>('sign-in', value)
+      //   .then(({ message }) => {
+      //     sign.emit('signInSuccess', { message })
+      //   })
+      sign.emit('signInSuccess', { message: 'OK' })
+    },
+    ({ email }: SignIn) => {
+      return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email)
+    },
+    (abort) => {
+      sign.emit('signInError', { message: 'Email inválido' })
+      // abort.dispatchEvent(new Event('abort'))
+    }
+  )
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { sign } from './domain/sign/api'
 import { query } from './core/utils/query'
 
-query<'button'>('#sign-in').onclick = () => {
-  sign.emit('validate', { email: 'hello@guiseek.dev' })
-  sign.emit('signIn', { email: 'hello' })
-  sign.on('validate', (value) => console.log(value))
-  sign.on('validate', (value) => console.log(value))
-}
+sign.on('signInSuccess', (value) => {
+  console.log(value)
+})
 
-query<'button'>('#sign-up').onclick = () => {
-  sign.emit('signUp', { name: 'Gui Seek', email: 'hello@guiseek.dev' })
+sign.on('signInError', (value) => {
+  console.log(value)
+})
+
+query<'button'>('#sign-in-ok').onclick = () => {
+  sign.emit('signIn', { email: 'hello@guiseek.dev' })
+}
+query<'button'>('#sign-in-fail').onclick = () => {
+  sign.emit('signIn', { email: 'hello' })
 }
